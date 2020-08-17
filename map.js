0daa9e8ebabb9ca48c40b563d88f33ad1915a7a6
Donghyeon Lee <jsleeg7279@naver.com>
2020-08-17 16:04:19 +0900

    kakao로 지도 변경

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ var mapOptions= {
         center:new naver.maps.LatLng(37.266444,126.997219), //초기위치
         zoom:15,
     };
-
+var mapcontainer = document.getElementById('map');
 function showMarker(lat,lng) {
 var marker = new naver.maps.Marker({
     position: new naver.maps.LatLng(lat,lng),
@@ -12,7 +12,7 @@ var marker = new naver.maps.Marker({
 
 var infoWindow = new naver.maps.InfoWindow();
 
-var map=new naver.maps.Map('map',mapOptions);  
+var map=new kakao.maps.Map(mapcontainer,mapOptions);  
 
 
     
