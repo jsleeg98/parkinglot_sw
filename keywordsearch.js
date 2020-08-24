0ba6350ec9d55a065b0ed881be36e744eab2c2f9
Donghyeon Lee <jsleeg7279@naver.com>
2020-08-24 10:56:40 +0900

    kakao test html 추가

---
+++ keywordsearch.js
@@ -0,0 +1,43 @@
+var k_infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+var k_mapContainer = document.getElementById('k_map'), // 지도를 표시할 div 
+    k_mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };  
+
+// 지도를 생성합니다    
+var k_map = new kakao.maps.Map(k_mapContainer, k_mapOption); 
+
+
+var ps = new kakao.maps.services.Places();
+
+ps.keywordSearch('이태원 맛집', palceSearchCB);
+
+function placesSearchCB(data, status, pagination){
+    if (status === kakao.maps.services.status.OK){
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for(var i=0; i<data.length; i++){
+            displayMarker(data[i]);    
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+        map.setBounds(bounds);
+    }
+}
+
+function displayMarker(place) {
+    
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x) 
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+    });
+}
