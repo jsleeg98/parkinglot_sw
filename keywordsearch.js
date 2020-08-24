97aa65204718a5a8484fc91b71b8fdcaccbc9020
Donghyeon Lee <jsleeg7279@naver.com>
2020-08-24 11:13:02 +0900

    kakao keywordsearch k_로 수정

--- keywordsearch.js
+++ keywordsearch.js
@@ -22,22 +22,22 @@ function placesSearchCB(data, status, pagination){
             displayMarker(data[i]);    
             bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
         }
-        map.setBounds(bounds);
+        k_map.setBounds(bounds);
     }
 }
 
 function displayMarker(place) {
     
     // 마커를 생성하고 지도에 표시합니다
-    var marker = new kakao.maps.Marker({
-        map: map,
+    var k_marker = new kakao.maps.Marker({
+        k_map: k_map,
         position: new kakao.maps.LatLng(place.y, place.x) 
     });
 
     // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, 'click', function() {
+    kakao.maps.event.addListener(k_marker, 'click', function() {
         // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-        infowindow.open(map, marker);
+        k_infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        k_infowindow.open(k_map, k_marker);
     });
 }
